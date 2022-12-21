23d2617209fa5b0fd5923d10f0ec96911ee5db8f
atharvaat <atharvatodankar.at@gmail.com>
2022-12-21 08:33:01 +0530

    Adding program to refactor code to ordered linkedlist

---
+++ src/main/java/com/datastructure/linkedlist/Main.java
@@ -0,0 +1,47 @@
+package com.datastructure.linkedlist;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to LinkedList DataStructure Program :");
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start"); // UC 2
+        System.out.println("Enter 2 to add the data at the End"); // UC 3
+        System.out.println("Enter 3 to add the data at In Between ");
+        System.out.println("Enter 4 to delete the data at first position");
+        System.out.println("Enter 5 to delete the data at last position ");
+        System.out.println("Enter 6 to search the element in linkedlist ");
+        System.out.println("Enter 7 to insert  the data at index position ");
+        System.out.println("Enter 8 to delete 40 from list ");
+        switch (scanner.nextInt()) {
+
+            case 1:
+                Operation.addDataAtStart();
+                break;
+            case 2:
+                Operation.addDataAtEnd();
+                break;
+            case 3:
+                Operation.insertInBetween();
+                break;
+            case 4:
+            	Operation.pop();
+            	break;
+            case 5:
+            	Operation.popLast();
+            	break;
+            case 6:
+            	Operation.search();
+            	break;
+            case 7:
+            	Operation.insertAtSpecificIndex();
+            	break;
+            case 8:
+                Operation.deleteAtIndexPosition();
+                break;
+	}
+        scanner.close();
+	}    
+}
